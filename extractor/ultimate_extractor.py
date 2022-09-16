06c0b730ead4b3fa0f076785a0a8383f6b70d45f
Pedro De Melo Milet <Pedro.Milet@encora.com>
2022-09-16 13:24:37 -0300

    Extrator final para entrega 2

---
+++ extractor/ultimate_extractor.py
@@ -0,0 +1,33 @@
+from ast import If
+import json
+import requests
+from bs4 import BeautifulSoup
+from  specifics.bigboygames import bigboyextractor
+from  specifics.vnsgames import vnsextractor
+
+from  specifics.futuristicGames import futuristicextractor
+from  specifics.lojaarenagames import arenaextractor
+from  specifics.shockgames import shockextractor
+from  specifics.ibyte import ibyteextractor
+from  specifics.shopb import shopbextractor
+from  generic import generic
+
+def extractor(url,html):
+    soup = BeautifulSoup(html, 'html.parser')
+    if "bigboy" in url:
+        return bigboyextractor(soup)
+    if "ibyte" in url:
+        # ibyteextractor
+        return ibyteextractor(soup)
+        # continue
+    # if "mercadolivre" in url:
+    #     return mercadolivreextractor(url)
+    if "shockgames" in url:
+        # vnsextractor
+        return shockextractor(soup)
+    if "shopb" in url:
+        # vnsextractor
+        return shopbextractor(soup)
+    if "vnsgames" in url:
+        # vnsextractor
+        return vnsextractor(soup)
