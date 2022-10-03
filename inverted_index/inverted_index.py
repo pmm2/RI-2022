ce3cc4e57ea3959655517fafb35cc14aaee2c9ce
Pedro De Melo Milet <Pedro.Milet@encora.com>
2022-10-03 14:45:54 -0300

    inverted index upgraded

--- inverted_index/inverted_index.py
+++ inverted_index/inverted_index.py
@@ -2,32 +2,29 @@ from asyncore import read
 from re import L, sub
 import re
 import pprint
+import json
 import numpy
 pp = pprint.PrettyPrinter(indent=4)
 
 
 def clean(doc):
-    doc = re.sub(r'[\W_]+', ' ', doc)
+    doc = re.sub(r'[\W_]+', '', doc)
     doc = doc.strip().lower()
     return doc
-
 
 def pick_quartis(price_list):
     q1 = numpy.quantile(price_list, q=0.25)
     q2 = numpy.quantile(price_list, q=0.5)
     q3 = numpy.quantile(price_list, q=0.75)
 
-
 # print(clean("I-like-bananas - "))
-
 
 class invertedIndex:
 
     def __init__(self, documents):
         self.vocabulary = dict()
         self.frequency = []
-        # roda clean para todos os documentos
-        self.documents = [[clean(d) for d in ds]for ds in documents]
+        self.documents = documents
         return
 
     def countWords(self, docIndex, word, occurences):
@@ -36,8 +33,10 @@ class invertedIndex:
             self.vocabulary.update({word: []})
             self.vocabulary[word].append([docIndex, occurences])
 
+
         else:
             self.vocabulary[word].append([docIndex, occurences])
+
 
     def indexCompression(self):
 
@@ -51,6 +50,7 @@ class invertedIndex:
                 self.vocabulary[entry][index][0] = l[index]
 
     def createInvertedIndex(self):
+
         docIndex = 1
         for document in self.documents:
 
@@ -64,16 +64,38 @@ class invertedIndex:
                           for v in self.vocabulary.keys()]
 
 
-teste = []
-p = 'I like Bananas120102'
-teste.append(len(p.lower().strip().split(' ')))
+# teste = []
+# p = 'I like Bananas120102'
+# teste.append(len(p.lower().strip().split(' ')))
 
-pag1 = 'To pedro to Rato To'.split()
-pag2 = 'to pedro Rato rato To TO'.split()
-pag3 = 'I  rato think therefore I am Do be do be do'.split()
-pag4 = 'Do pedro rato rato rato do do da da da Let it be let it be'.split()
-ii = invertedIndex([pag1, pag2, pag3, pag4])
+# pag1 = 'To pedro to Rato To'.split()
+# pag2 = 'to pedro Rato rato To TO'.split()
+# pag3 = 'I  rato think therefore I am Do be do be do'.split()
+# pag4 = 'Do pedro rato rato rato do do da da da Let it be let it be'.split()
+# ii = invertedIndex([pag1, pag2, pag3, pag4])
+# ii.createInvertedIndex()
+# pp.pprint(ii.vocabulary)
+# pp.pprint(ii.frequency)
+# ii.indexCompression()
+
+# pp.pprint(ii.vocabulary)
+f = open('./data/data_map.json')
+data = json.load(f)
+documentos = []
+def tag (s,tag):
+    cleaned = [ f"{clean(i)}" for i in s if i != 'None']
+    tagged = [ f"{i}.{tag}" for i in cleaned if len(i)>0]
+    return tagged
+for i in data:
+    doc = []
+    titleList = tag(data[i]['title'].split(),'title')
+    genreList = tag(data[i]['genre'].split(),'genre')
+    plataformaList = tag(data[i]['plataforma'].split(),'plataforma')
+    devList = tag(data[i]['dev'].split(),'dev')
+    docList =titleList+genreList+plataformaList+devList
+    documentos.append(docList)
+f.close()
+
+ii = invertedIndex(documentos)
 ii.createInvertedIndex()
-pp.pprint(ii.vocabulary)
-ii.indexCompression()
 pp.pprint(ii.vocabulary)
