339c5b94c58a9b7f6134684a6f164393de83456f
JoaoPlacido <jpsn@cin.ufpe.br>
2022-10-03 20:31:52 -0300

    modulo de busca funcionando(tomara)

--- search/search.py
+++ search/search.py
@@ -25,53 +25,50 @@ def execute_query(query_string, title, gen, dev, plat, price, num_docs, docs_len
     if title != '':
 
         games = run_query(title, tfidf, num_docs,
-                          docs_lenght, n, field='title')
+                          docs_lenght, n, field='title', invlist_path='./inverted_index/fields_invindex.json')
         games_list.append(games)
 
     if gen != '':
 
         games = run_query(
-            gen, tfidf, num_docs, docs_lenght, n, field='gen')
+            gen, tfidf, num_docs, docs_lenght, n, field='genre', invlist_path='./inverted_index/fields_invindex.json')
         games_list.append(games)
 
     if dev != '':
 
         games = run_query(
-            dev, tfidf, num_docs, docs_lenght, n, field='dev')
+            dev, tfidf, num_docs, docs_lenght, n, field='dev', invlist_path='./inverted_index/fields_invindex.json')
         games_list.append(games)
 
     if plat != '':
 
         games = run_query(
-            plat, tfidf, num_docs, docs_lenght, n, field='plat')
+            plat, tfidf, num_docs, docs_lenght, n, field='plataforma', invlist_path='./inverted_index/fields_invindex.json')
         games_list.append(games)
 
     if price != '':
 
         games = run_query(
-            price, tfidf, num_docs, docs_lenght, n, field='price')
+            price, tfidf, num_docs, docs_lenght, n, field='price', invlist_path='./inverted_index/fields_invindex.json')
         games_list.append(games)
 
+    # print(games_list)
     all_games = {}
-
     for ranks_list in games_list:
         for game in ranks_list:
-            if not str(game[0]) in all_games:
-                all_games[str(game[0])] = float(game[1])
+            if not str(game) in all_games:
+                all_games[str(game)] = float(ranks_list[game])
             else:
-                all_games[str(game[0])] += float(game[1])
+                all_games[str(game)] += float(ranks_list[game])
     for doc in all_games:
         all_games[doc] /= 6
-
-    return sorted(all_games.items(), key=operator.itemgetter(1), reverse=True)
+    print(all_games)
+    return sorted(all_games)
 
 
 # calcular no começo de tudo
-def get_docs_len(docs_path_map='./data/data_map.json'):
+def get_docs_len(map_docs):
     lengths = {}
-    with open(docs_path_map, 'r') as map:
-        map_docs = map.read()
-        map_docs = json.loads(map_docs)
     for id in map_docs:
         with open(f"./data/htmls/{map_docs[id]['arquivo']}.html") as html:
             lengths[id] = len(BeautifulSoup(
@@ -83,21 +80,24 @@ def cosine_Score(terms, tfidf, invlist, num_docs, doc_lengths, n):
     scores = {i: 0 for i in range(1, num_docs + 1)}
     for term in terms:
 
-        w_term_query = term.count(term)
+        w_term_query = terms.count(term)
         if (tfidf):
             w_term_query = 0.5 + 0.5 * \
                 (w_term_query /
-                 max([i for i, val in enumerate(terms) if val == term])+1)
+                 (max([i for i, val in enumerate(terms) if val == term])+1))
             w_term_query *= get_idf(invlist[term], num_docs)
-        for post in term:
+        for post in invlist[term]:
+            # print(post)
             w_doc_term = post[1]  # tf
             if tfidf:
                 w_doc_term *= get_idf(invlist[term], num_docs)
             scores[post[0]] += w_term_query * w_doc_term
-    for d in range(1, num_docs + 1):
-        scores[d] /= doc_lengths[d]
-    scores = sorted(scores.items, key=lambda x: x[1], reverse=True)
-    return scores[:n]
+    for d in range(num_docs):
+        # print(doc_lengths)
+        scores[d+1] /= doc_lengths[f'{d+1}']
+    keys_scores = sorted(scores, key=scores.get, reverse=True)
+    # print(scores)
+    return {keys_scores[i]: scores[keys_scores[i]] for i in range(5)}
 
 
 def clear_query_input(query, field=None, usestopwords=False):
@@ -143,7 +143,21 @@ def get_invindex(path='./inverted_index/invIndex.json'):
     return invindex
 
 
-def run_query(query, tfidf, num_docs, docs_lenght, n, field=None, invlist_path='./inverted_index/invIndex.json'):
+def run_query(query, tfidf, num_docs, docs_lenght, n, field=None, invlist_path='./inverted_index/geral_invindex.json'):
     terms = clear_query_input(query, field)
     invindex = get_invindex(invlist_path)
     return cosine_Score(terms, tfidf, invindex, num_docs, docs_lenght, n)
+
+
+# data_map = {}
+# with open('./data/data_map.json') as infile:
+#     text = infile.read()
+#     data_map = json.loads(text)
+
+# qt_docs = len(data_map)
+# docs_len = get_docs_len(data_map)
+# print(execute_query(query_string='mario', title='', gen='',
+#       dev='', plat='', price='', num_docs=qt_docs, docs_lenght=docs_len))
+
+# print(execute_query(query_string='', title='ps4', gen='esportes',
+#       dev='', plat='', price='', num_docs=qt_docs, docs_lenght=docs_len))
